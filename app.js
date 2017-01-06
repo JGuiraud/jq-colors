1c503b8f1ec0a2585fe0d9f737799867bf9059b7
JGuiraud <jerome31guiraud@gmail.com>
2017-01-06 13:14:07 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// good luck !
+$(document).ready(function(){
+   
+    var boutons = $("[type='button']");
+    var couleurforbackgrd
+   
+    for (i=0; i<11; i++) {
+        var bouton = $(boutons[i]);
+        var couleurbouton = bouton.data("color");
+        bouton.css( "background-color", couleurbouton);
+    };
+
+    boutons.click(function(){
+        couleurforbackgrd = ($(this).data("color"));
+        console.log(couleurforbackgrd);
+        $("body").css("background", couleurforbackgrd);
+        });
+        
+    var checkbox = $("[type='checkbox']")
+    checkbox.click(function(){
+    if ($(":checkbox").is(":checked")) {
+            $(".main").css("color", couleurforbackgrd);
+        } else {
+            $(".main").css("color", "black");
+        }
+    });
+});
